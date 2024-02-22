9ddb9e6db9623f272955634cc3494f1a0177f092
Wieland Morgenstern <wieland.morgenstern@hhi.fraunhofer.de>
2024-02-22 19:37:45 +0100

    README and img sorting example

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
-# PLAS
-Parallel Linear Assignment Sorting
+# Parallel Linear Assignment Sorting (PLAS)
+Sorting multi-dimensional data into locally smooth 2D grids.
+
+The algorithm will sort tensors of shape *(C, n, n)*. It reorders the elements along the last two dimensions (grid columns and rows) with the attempt to minimize the $L^2$-distance between neighboring elements in $x$ and $y$ direction.
+
+### Example
+
+We can use PLAS to sort the pixels of an image, where *C=3* from the three color channels of the image. An example implementation can be found in `sort_rgb_img.py`.
+
+*Starry Night* by Vincent van Gogh             |  *Sorted Night* with PLAS
+:-------------------------:|:-------------------------:
+![A reproduction of the painting Starry Night by Vincent van Gogh](/img/VanGogh-starry_night.jpg)  | ![All pixels of the painting sorted with the algorithm](/img/VanGogh-starry_night_sorted.png)
+
+The sorted output contains the same pixels as the input image, reordered to have high similarity between neighbors.
 
 
+### Preparing data
 
-Shuffle
+Currently only square grids are supported in the implementation. Data needs to be truncated and reshaped into shape *(C, n, n)* before being passed into the sorting method.
+
+If the input data is already partially ordered, you may consider shuffling it before calling PLAS, to avoid getting stuck in local minima. But given the iterative nature of the algorithm, a random shuffle will also increase the time to sort significantly.
---
+++ sort_rgb_img.py
@@ -0,0 +1,53 @@
+# Load an image and sort it using the PLAS algorithm
+# The output is saved in the same directory as the input image, with the suffix "_sorted.png" added to the filename.
+
+from PIL import Image
+from plas import sort_with_plas
+import math
+import torch
+import torchvision
+import click
+import os
+
+@click.command()
+@click.option("--img-path", type=click.Path(exists=True))
+@click.option("--shuffle/--no-shuffle", default=True)
+def sort_image(img_path, shuffle):
+
+    if torch.backends.mps.is_available():
+        device = torch.device("mps")
+    elif torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = "cpu"
+
+    print(f"Using device: {device}")
+
+    # Read an image and put it on the GPU
+    image = Image.open(img_path).convert("RGB")
+    tensor_image = torchvision.transforms.ToTensor()(image).to(device)
+
+    # Currently only support square images, so
+    sidelen = int(math.sqrt(tensor_image.shape[1] * tensor_image.shape[2]))
+
+    # truncate the image
+    img_trunc = tensor_image.flatten(start_dim=1)[:, :sidelen * sidelen]
+
+    if shuffle:
+        # shuffle the image to avoid local minimum
+        img_trunc = img_trunc[:, torch.randperm(sidelen * sidelen)]
+
+    # reshape the image to be a square
+    img_trunc_shuf_sq = img_trunc.reshape(-1, sidelen, sidelen)
+
+    sorted_img, grid_indices = sort_with_plas(img_trunc_shuf_sq, improvement_break=1e-4, verbose=True)
+
+    output_file = os.path.basename(img_path).split(".")[0]
+    if shuffle:
+        output_file += "_shuffled"
+    output_file += "_sorted.png"
+
+    torchvision.utils.save_image(sorted_img, os.path.join(os.path.dirname(img_path), output_file))
+
+if __name__ == "__main__":
+    sort_image()
