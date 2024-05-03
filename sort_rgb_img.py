a928fbc67c1e156bd227a532f7672e8d730a1430
Wieland Morgenstern <wieland.morgenstern@hhi.fraunhofer.de>
2024-05-03 18:15:44 +0200

    print VAD in image example

--- sort_rgb_img.py
+++ sort_rgb_img.py
@@ -9,6 +9,8 @@ import torchvision
 import click
 import os
 
+from vad import compute_vad
+
 @click.command()
 @click.option("--img-path", type=click.Path(exists=True))
 @click.option("--shuffle/--no-shuffle", default=True)
@@ -33,9 +35,16 @@ def sort_image(img_path, shuffle):
     # truncate the image
     img_trunc = tensor_image.flatten(start_dim=1)[:, :sidelen * sidelen]
 
+    # TODO: not really a useful vad with the truncated reshaped image
+    # -> fix after supporting non-square images
+    vad_img_trunc = compute_vad(img_trunc.reshape(-1, sidelen, sidelen).permute(1, 2, 0).cpu().numpy() * 255)
+
     if shuffle:
         # shuffle the image to avoid local minimum
         img_trunc = img_trunc[:, torch.randperm(sidelen * sidelen)]
+
+        # TODO: see above for non-square images
+        vad_img_trunc_shuf = compute_vad(img_trunc.reshape(-1, sidelen, sidelen).permute(1, 2, 0).cpu().numpy() * 255)
 
     # reshape the image to be a square
     img_trunc_shuf_sq = img_trunc.reshape(-1, sidelen, sidelen)
@@ -48,6 +57,13 @@ def sort_image(img_path, shuffle):
     output_file += "_sorted.png"
 
     torchvision.utils.save_image(sorted_img, os.path.join(os.path.dirname(img_path), output_file))
+
+    vad_sorted_img = compute_vad(sorted_img.permute(1, 2, 0).cpu().numpy() * 255)
+
+    print(f"VAD of image: {vad_img_trunc:.4f}")
+    if shuffle:
+        print(f"VAD of shuffled image: {vad_img_trunc_shuf:.4f}")
+    print(f"VAD of sorted image: {vad_sorted_img:.4f}")
 
 if __name__ == "__main__":
     sort_image()
