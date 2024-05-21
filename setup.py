e4e83cea1b0501dd026ba47d5a0717897d71a004
Wieland Morgenstern <wieland.morgenstern@hhi.fraunhofer.de>
2024-05-21 16:33:42 +0200

    setup.py pip package

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='plas',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'torch',
+        'kornia',
+        'tqdm',
+
+        # example/eval deps, maybe split?
+        # sort_3d_gaussians
+        'pandas',
+        'plyfile',
+        'trimesh',
+
+        # sort_rgb_img
+        'click',
+        'pillow',
+
+
+        # eval/compare_plas_flas
+        'opencv-python',
+
+        # eval/flas
+        'las',
+        'matplotlib'
+    ],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+    package_data={
+        'plas': ['../img/*.jpg'],
+    },
+    include_package_data=True,
+)
